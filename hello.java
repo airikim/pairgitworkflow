74fb3cf9e130165240fad3e39b629c840593fbd7
NewWorld <newworld@server.skbroadband>
2022-07-01 15:20:52 +0900

    First Pair Commit

--- hello.java
+++ hello.java
@@ -1,1 +1,9 @@
 hello...
+
+Static void main(String[] args)
+
+	printf(HelloJava);
+
+class HelloJva{
+	printf("아리님 안녕하세요");
+	return;
---
+++ hi.py
@@ -0,0 +1,3 @@
+class HelloPython
+	print("아리님 안녕하세요!")
+	return
